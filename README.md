d3dd9ec754a5a42d67686dafc1f6f1379402767a
pajamaw <pjpeterww@gmail.com>
2016-06-20 20:39:13 -0400

    Completed Puzzle after creating function that cleaned up triangle.txt file into a nested array. Allows you to solve puzzle, using dataformat of the text file given by simply inputting the text.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Built for Yodle!
+By PJ Wickwire
+720 255 8835
+github.com/pajamaw
--- trig.js
+++ trig.js
@@ -1,13 +1,55 @@
-var arrayofnumbers = [[1],[1, 2], [1, 2, 3,]]
+fs = require('fs');
+//theNumbers = null;
+//lineByLine = null;
+//lineBy2 = [];
+//filepath = '/Users/pjwickwire/Development/Interviews/yodle-triangle/triangle.txt'
+//Here I save the text of the file into a global variable
+function getFile(filepath){
+  fs.readFile(filepath, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    //console.log(data);
+    theNumbers = data;
+  });
+  return theNumbers;
+};
 
+//Here i pass the newly saved variable theNumbers to create an array
+//that sorts every line of the pyramid into its own string element
+function makeManageable(triangletxt){
+  lineByLine = triangletxt.split(/\n/);
+  console.log(lineByLine);
+  return lineByLine;
+  //iterate through each line then split each line into another array by \s
+}
+//Next we pass  lineByLine , and iterate through each element sorting every element into a new array based on how many specific numbers are
+//in teh string of text. so now we will have [[1], [1, 2], [1, 2, 3]...etc]
+//it will also give us extra elements of white space - but that's fine as it won't mess up the
+//actual method.
+function makeFinished(array){
+  lineBy2 = [];
+  for(i = 0; i < array.length; i++ ){
+    console.log(array[i].split(/\s/));
+    lineBy2.push(array[i].split(/\s/));
+  }
+  return lineBy2;
+}
+//A simple add method for a final total
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+//And the main puzzle itself, I use two loops to iterate through the newly formatted
+//nested array it selects the highest number in each array than pushes that number to
+//a newly created array to store the infor for later.
 function triPuzzle(numb){
   var largest=0;
   var la = [];
   for (i = 0; i < numb.length; i++){
     for (b = 0; b < numb[i].length; b++){
-      if (numb[i][b] > largest){
+      if (parseInt(numb[i][b]) > largest){
         //console.log(largest);
-        largest = numb[i][b]
+        largest = parseInt(numb[i][b])
       };
     };
     la.push(largest);
@@ -17,7 +59,15 @@ function triPuzzle(numb){
     //console.log(largest);
     //console.log(la)
   };
-  return la;
+  console.log(la);
+
+  var sum = la.reduce(add, 0);
+  console.log("The sum of the numbers is " + sum);
+  console.log("The length of the array is " + la.length);
 }
 
-var arrayofnumbers = [[1],[1, 2], [1, 2, 3,]]
+function completePuzzle(filepath){
+  triPuzzle(makeFinished(makeManageable(getFile(filepath))));
+}
+
+//completePuzzle('/Users/pjwickwire/Development/Interviews/yodle-triangle/triangle.txt')
